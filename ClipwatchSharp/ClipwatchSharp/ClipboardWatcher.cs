b77d2fb563f1458465d45c1eda0927ef64af8dc7
milleniumbug <milleniumbug@protonmail.com>
2022-01-01 17:35:00 +0100

    Prevent callbacks from being garbage collected

--- ClipwatchSharp/ClipwatchSharp/ClipboardWatcher.cs
+++ ClipwatchSharp/ClipwatchSharp/ClipboardWatcher.cs
@@ -14,19 +14,23 @@ namespace ClipwatchSharp
             gcHandle.Free();
         }
 
+        private static NativeFunctions.UserDataDeleteCallback FreeContextDelegate = FreeContext;
+
         private static unsafe void Callback(byte* buffer, UIntPtr length, IntPtr userData)
         {
             var gcHandle = GCHandle.FromIntPtr(userData);
             var context = (ClipboardWatcher)gcHandle.Target;
             context.ClipboardChanged?.Invoke(context, Encoding.UTF8.GetString(buffer, (int)length.ToUInt32()));
         }
+
+        private static unsafe NativeFunctions.UserCallback CallbackDelegate = Callback;
         
         public event EventHandler<string> ClipboardChanged;
 
         public unsafe ClipboardWatcher(int pollingIntervalInMs = 500)
         {
             var gcHandle = GCHandle.Alloc(this);
-            this.handle = NativeFunctions.Clipwatch_Init(pollingIntervalInMs, Callback, GCHandle.ToIntPtr(gcHandle), FreeContext);
+            this.handle = NativeFunctions.Clipwatch_Init(pollingIntervalInMs, CallbackDelegate, GCHandle.ToIntPtr(gcHandle), FreeContextDelegate);
         }
 
         public void Start()
